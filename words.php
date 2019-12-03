98452aa8e607f572edf6d6d4dc603636650c5e11
Jeffery Gontarek <jeffery@Jefferys-MacBook-Pro.local>
2019-12-02 20:16:32 -0800

    Reverse word

--- words.php
+++ words.php
@@ -2,4 +2,12 @@
 echo "Enter a word:";
 $input = rtrim(fgets(STDIN));
 $strlength = strlen($input);
+$strreverse = strrev($input);
 echo "Your word is {$strlength} letters long\n";
+if ($strlength > 5) {
+echo "{$strreverse}\n";
+}else {
+echo "{$input}\n";
+}
+?>
+
